fa48484e03006d88572e3f3cee389ff7526e5817
Daniellyson <etu32290@henallux.be>
2018-04-03 15:07:18 +0200

    model

---
+++ model/Main.java
@@ -0,0 +1,10 @@
+package model;
+
+import view.ViewPrincipal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ViewPrincipal viewPrincipal = new ViewPrincipal();
+    }
+}
