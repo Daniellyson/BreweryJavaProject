94267f753fd9557083889b8b57b4b88a4cdec60b
Daniellyson <etu32290@henallux.be>
2018-04-13 11:00:43 +0200

    ajout des classes modele

---
+++ model/OrderLine.java
@@ -0,0 +1,44 @@
+package model;
+
+public class OrderLine {
+    private String lineCode;
+    private Integer lineNumber;
+    private Integer price;
+    private Integer reductionPercentage;
+    private Integer amount;
+    private Order order;
+    private Sale sale;
+    private Product product;
+
+    public void setLineCode(String lineCode) {
+        this.lineCode = lineCode;
+    }
+
+    public void setLineNumber(Integer lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public void setReductionPercentage(Integer reductionPercentage) {
+        this.reductionPercentage = reductionPercentage;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public void setSale(Sale sale) {
+        this.sale = sale;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
