145818ca32030316c53425741859a9c7cdabd031
Daniellyson <etu32290@henallux.be>
2018-04-22 08:36:47 +0200

    tables update

--- data_Base/Data_Base_Brewery.sql
+++ data_Base/Data_Base_Brewery.sql
@@ -7,17 +7,22 @@ drop table costumer;
 drop table city;
 */
 
-/*Doing auto_increment*/
+
+SET @@GLOBAL.auto_increment_increment = 10;
+/*SET @@SESSION.auto_increment_increment = 10;*/
+
 
 create table City 
-	 (Code int(7) primary key,
+	 (Code int(7) primary key auto_increment,
      Name varchar(30) not null,
      PostCode integer(5) not null,
      unique(Name, PostCode)) ENGINE=INNODB;
-     
+alter table City auto_increment = 10;
+
+
      
 create table Costumer
-	(CostomerNumber varchar(7) primary key,
+	(CostomerNumber varchar(7) primary key auto_increment,
      LastName varchar(25) not null,
      FirstName varchar(15) not null,
      FirstName2 varchar(15),
@@ -32,15 +37,17 @@ create table Costumer
      DateOfBirth date not null,
      Code int(7) not null, 
      foreign key fk_City (Code) references City(Code)) ENGINE=INNODB;
+alter table Costumer auto_increment = 10;
      
 
 create table ProductOrder
-	(OrderCode varchar(7) primary key,
+	(OrderCode varchar(7) primary key auto_increment,
      CreationDate date not null,
      TargetDate date not null,
      CostomerNumber varchar(7) not null,
      foreign key fk_Costomer (CostomerNumber) references Costumer(CostomerNumber)) ENGINE=INNODB;
-     
+alter table ProductOrder auto_increment = 10;
+
 
 create table Sale
 	(SaleCode varchar(7) primary key,
@@ -48,7 +55,8 @@ create table Sale
      TargetDate date not null,
      CostomerNumber varchar(7) not null,
      foreign key fk_CustomerNumber(CostomerNumber) references Costumer(CostomerNumber)) ENGINE=INNODB;
-     
+alter table Sale auto_increment = 10;
+
      
 create table Product
 	(ProductCode varchar(7) primary key,
@@ -56,6 +64,7 @@ create table Product
      Price decimal(6,2) not null,
      ReductionPercentage decimal(5,2) not null,
      AmountInventory int(7) not null) ENGINE=INNODB;
+alter table Product auto_increment = 10;
 
      
 create table OrderLine
@@ -70,5 +79,5 @@ create table OrderLine
      foreign key fk_Order (OrderCode) references ProductOrder(OrderCode),
      foreign key fk_Sale (SaleCode) references Sale(SaleCode),
      foreign key fk_Product (ProductCode) references Product(ProductCode)) ENGINE=INNODB;
-
+alter table OrderLine auto_increment = 10;
 
