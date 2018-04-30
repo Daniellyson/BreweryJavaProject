8d35082f395f66a69627db77f05da26e1f3a6c9e
Daniellyson <etu32290@henallux.be>
2018-04-30 21:52:35 +0200

    update

--- thread/ThreadAnimation.java
+++ thread/ThreadAnimation.java
@@ -1,21 +1,18 @@
 package thread;
 
 import view.Images;
+import view.ViewPrincipal;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class ThreadAnimation extends Thread {
-    private  Container container;
+    private ViewPrincipal viewPrincipal;
     private static boolean keepGoing;
 
-    public ThreadAnimation(Container container, boolean keepGoing) {
-        this.container = container;
-        this.keepGoing = keepGoing;
-    }
-
-    public ThreadAnimation(Container container) {
-        this(container, true);
+    public ThreadAnimation( ViewPrincipal viewPrincipal) {
+        this.viewPrincipal = viewPrincipal;
+        keepGoing = true;
     }
 
     public ThreadAnimation(boolean keepGoing) {
@@ -25,22 +22,15 @@ public class ThreadAnimation extends Thread {
     public void run() {
         //TODO doing thread
         Images img = new Images();
-        JLabel frame = new JLabel();
         try {
             while (keepGoing) {
                 Thread.sleep(70);
-                ImageIcon imageIcon = img.changeFrame();
-                frame.setIcon(imageIcon);
-                container.remove(frame);
-                frame.setHorizontalAlignment(SwingConstants.CENTER);
-                container.add(frame, BorderLayout.SOUTH);
-                container.revalidate();
-                container.repaint();
-                System.out.println("KEEP");
+                ImageIcon imageIcon = img.getFrame();
+                viewPrincipal.changeFrameView(imageIcon);
+                viewPrincipal.repaint();
             }
             if(!keepGoing) {
-                System.out.println("STOP");
-                container.remove(frame);
+                viewPrincipal.changeFrameView(null);
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
