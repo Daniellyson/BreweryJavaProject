24364c704636b5da07a73053c8f0acddd2bf8be5
Daniellyson <etu32290@henallux.be>
2018-05-06 16:57:04 +0200

    update

--- controller/ApplicationController.java
+++ controller/ApplicationController.java
@@ -1,8 +1,8 @@
 package controller;
 
 import businessLogic.CustomerManager;
-import exception.AddCostumerException;
-import exception.GetCostumerException;
+import exception.AddCustomerException;
+import exception.GetCustomerException;
 import model.Customer;
 
 import java.util.ArrayList;
@@ -14,11 +14,11 @@ public class ApplicationController {
         customerManager = new CustomerManager();
     }
 
-    public ArrayList<Customer> getAllCustomers() throws GetCostumerException {
+    public ArrayList<Customer> getAllCustomers() throws GetCustomerException {
         return customerManager.getAllCustomers();
     }
 
-    public void addCustomer(Customer customer) throws AddCostumerException {
+    public void addCustomer(Customer customer) throws AddCustomerException {
         customerManager.addCustomer(customer);
     }
 }
